7e6bb31836e9ef72b517a16651fffa2da1f36a42
andhmak <sdi1900047@di.uoa.gr>
2020-06-10 18:03:36 +0300

    Documentation και προσθήκη μερικών tests

--- tests/ADTGraph_test.c
+++ tests/ADTGraph_test.c
@@ -13,13 +13,13 @@ int compare_vertices(Pointer a, Pointer b) {
 	return !(a == b);
 }
 
-// Προς υλοποίηση
-
 void test_create() {
 	Graph graph = graph_create(compare_vertices, free);
 	graph_set_hash_function(graph, hash_pointer);
+
 	TEST_ASSERT(graph != NULL);
 	TEST_ASSERT(graph_size(graph) == 0);
+
 	graph_destroy(graph);
 }
 
@@ -58,12 +58,14 @@ void test_insert() {
 	for (int i = 0; i < N; i++) {
 		// Εισαγωγή, δοκιμή και έλεγχος ότι ενημερώθηκε το size
 		graph_insert_vertex(graph, vertex_array[i]);
+		// Βάζουμε και μια ακμή ανά δύο κορυφές
 		if (i) {
 			graph_insert_edge(graph, vertex_array[i - 1], vertex_array[i], i);
 		}
 		TEST_ASSERT(graph_size(graph) == (i + 1));
 	}
 
+	// Ελέγχουμε ότι υπάρχουν όλες οι κορυφές
 	List vertex_list = graph_get_vertices(graph);
 	TEST_ASSERT(list_size(vertex_list) == N);
 	for (int i = 0 ; i < N ; i++) {
@@ -71,6 +73,7 @@ void test_insert() {
 	}
 	list_destroy(vertex_list);
 
+	// Ελέγχουμε ότι μπήκαν σωστά όλες οι ακμές
 	List neighb_list = graph_get_adjacent(graph, vertex_array[0]);
 	TEST_ASSERT(list_size(neighb_list) == 1);
 	TEST_ASSERT(list_node_value(neighb_list, list_first(neighb_list)) == vertex_array[1]);
@@ -89,14 +92,17 @@ void test_insert() {
 	TEST_ASSERT(list_node_value(neighb_list, list_first(neighb_list)) == vertex_array[N - 2]);
 	list_destroy(neighb_list);
 
+	// Ελέγχουμε ότι είναι σωστά όλα τα βάρη
 	for (uint i = 0 ; i < N - 1 ; i++) {
 		TEST_ASSERT(graph_get_weight(graph, vertex_array[i], vertex_array[i + 1]) == i + 1);
 		TEST_ASSERT(graph_get_weight(graph, vertex_array[i + 1], vertex_array[i]) == i + 1);
 	}
 
+	// Βάζουμε μερικές ακόμα ακμές για να μην ελέγχουμε μόνο μια εκφυλισμένη λίστα
 	for (int i = 0 ; i < N - 2 ; i++) {
 		graph_insert_edge(graph, vertex_array[i], vertex_array[i + 2], i + 10);
 	}
+	// ΕΛέγχουμε ότι μπήκαν σωστά
 	for (uint i = 2 ; i < N - 2 ; i++) {
 		neighb_list = graph_get_adjacent(graph, vertex_array[i]);
 		TEST_ASSERT(list_size(neighb_list) == 4);
@@ -123,7 +129,7 @@ void test_remove() {
 	shuffle(vertex_array, N);
 	// Δοκιμάζουμε την insert εισάγοντας κάθε φορά νέους κόμβους
 	for (int i = 0; i < N; i++) {
-		// Εισαγωγή, δοκιμή και έλεγχος ότι ενημερώθηκε το size
+		// Εισαγωγή κορυφών και ακμών
 		graph_insert_vertex(graph, vertex_array[i]);
 		if (i) {
 			graph_insert_edge(graph, vertex_array[i - 1], vertex_array[i], i);
@@ -132,10 +138,11 @@ void test_remove() {
 
 	List neighb_list;
 	for (int i = N - 1; i >= 0; i--) {
-		// Εισαγωγή, δοκιμή και έλεγχος ότι ενημερώθηκε το size
+		// Αφαιρούμε κορυφές και ελέγχουμε ότι μειώνεται το μέγεθος
 		graph_remove_vertex(graph, vertex_array[i]);
 		TEST_ASSERT(graph_size(graph) == (i));
 
+		// Και πως όλος ο υπόλοιπος γράφος παραμένει ανεπηρέαστος
 		for (int k = 1 ; k < i - 1; k++) {
 			neighb_list = graph_get_adjacent(graph, vertex_array[k]);
 			TEST_ASSERT(list_size(neighb_list) == 2);
@@ -171,7 +178,7 @@ void test_remove() {
 	shuffle(vertex_array, N);
 
 	for (int i = 0; i < N; i++) {
-		// Εισαγωγή, δοκιμή και έλεγχος ότι ενημερώθηκε το size
+		// Εισαγωγή κορυφών και ακμών
 		graph_insert_vertex(graph, vertex_array[i]);
 		if (i) {
 			graph_insert_edge(graph, vertex_array[i - 1], vertex_array[i], i);
@@ -179,6 +186,8 @@ void test_remove() {
 	}
 
 	for (int i = 0 ; i  < N - 1; i++) {
+		// Ελέγχουμε ότι μαζί με τις κορυφές αφαιρούνται και οι ακμές
+		// οι γείτονες δηλαδή δεν "βλέπουν" τις αφαιρεμένες κορυφές
 		graph_remove_edge(graph, vertex_array[i], vertex_array[i + 1]);
 		neighb_list = graph_get_adjacent(graph, vertex_array[i]);
 		TEST_ASSERT(list_size(neighb_list) == 0);
@@ -204,22 +213,37 @@ void test_shortest_path() {
 	shuffle(vertex_array, N);
 	// Δοκιμάζουμε την insert εισάγοντας κάθε φορά νέους κόμβους
 	for (int i = 0; i < N; i++) {
-		// Εισαγωγή, δοκιμή και έλεγχος ότι ενημερώθηκε το size
+		// Εισαγωγή κορυφών και ακμών
 		graph_insert_vertex(graph, vertex_array[i]);
 		if (i) {
 			graph_insert_edge(graph, vertex_array[i - 1], vertex_array[i], i);
 		}
 	}
 
+	// Τρέχουμε Dijkstra στον γράφο-λίστα και ελέγχουμε πως λειτουργεί
 	List path = graph_shortest_path(graph, vertex_array[0], vertex_array[N - 1]);
 	TEST_ASSERT(list_size(path) == N);
 	int i;
 	ListNode node;
+	for (i = 0, node = list_first(path) ; i < N ; i++, node = list_next(path, node)) {
+		TEST_ASSERT(list_node_value(path, node) == vertex_array[i]);
+	}
+	list_destroy(path);
+
+	for (int i = 0; i < N - 2; i += 2) {
+		// Εισαγωγή επιπλέον ακμών
+		graph_insert_edge(graph, vertex_array[i], vertex_array[i + 2], i/2);
+	}
+
+	// Ελέγχουμε πώς ακολουθεί το πιο σύντομο μονοπάτι (ανά δύο)
+	path = graph_shortest_path(graph, vertex_array[0], vertex_array[N - 1]);
+	TEST_ASSERT(list_size(path) == (N + 2)/2);
 	for (i = 0, node = list_first(path) ; i < 0 ; i++, node = list_next(path, node)) {
 		TEST_ASSERT(list_node_value(path, node) == vertex_array[i]);
 	}
 	list_destroy(path);
-	
+
+	// Βάζουμε μια ακμή από την αρχή προς το τέλος και ελέγχουμε πως την χρησιμοποιεί
 	graph_insert_edge(graph, vertex_array[0], vertex_array[N - 1], 1);
 	path = graph_shortest_path(graph, vertex_array[0], vertex_array[N - 1]);
 	TEST_ASSERT(list_size(path) == 2);
@@ -227,13 +251,20 @@ void test_shortest_path() {
 	(list_node_value(path, list_next(path, list_first(path))) == vertex_array[N - 1]));
 	list_destroy(path);
 
+	// Απομονώνουμε τον πρώτο κόμβο και ελέγχουμε πως δεν βρίσκεται μονοπάτι
+	graph_remove_edge(graph, vertex_array[0], vertex_array[N - 1]);
+	graph_remove_edge(graph, vertex_array[0], vertex_array[1]);
+	graph_remove_edge(graph, vertex_array[0], vertex_array[2]);
+	path = graph_shortest_path(graph, vertex_array[0], vertex_array[N - 1]);
+	TEST_ASSERT(list_size(path) == 0);
+	list_destroy(path);
+	
 	graph_destroy(graph);
 	free(vertex_array);
 }
 
 // Λίστα με όλα τα tests προς εκτέλεση
 TEST_LIST = {
-	// { "create", test_create },
 
 	{ "graph_create", test_create },
 	{ "graph_insert", test_insert },
